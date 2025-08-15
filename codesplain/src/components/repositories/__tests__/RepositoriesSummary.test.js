acde85b33d546ab26d13bf268b3aac7e16efe261
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2025-08-15 10:39:55 -0400

    Section 7: Big Project Overview

---
+++ codesplain/src/components/repositories/__tests__/RepositoriesSummary.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import RepositoriesSummary from '../RepositoriesSummary';
+
+test('display information about the repositury', () => {
+  const repository = {
+    stargazers_count: 1,
+    open_issues: 50,
+    forks: 200,
+    language: 'Javascript',
+  };
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
+
+    expect(element).toBeInTheDocument();
+  }
+});
